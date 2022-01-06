642c8ca034e9b3efb578fe2731b379bc70465465
Beka93 <rebekafruhwirth@yahoo.de>
2022-01-06 14:24:56 +0100

    added functions to store

--- src/Form.svelte
+++ src/Form.svelte
@@ -1,135 +1,173 @@
 <script>
-    import { todos } from "./stores";
-    import { displayTodos } from "./stores";
-    import { filter } from "./stores";  
+  import { todos, displayTodos, filter, search } from "./stores";
+  import Fa from "svelte-fa/src/fa.svelte";
+  import {
+    faCheck,
+    faPlus,
+    faTrashAlt,
+  } from "@fortawesome/free-solid-svg-icons";
 
-    import Fa from "svelte-fa/src/fa.svelte";
-    import { faCheck, faPlus, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+  let task = "";
 
-    let task = '';
-  </script>
-  
-  
-  <main class="form">
-    <form
-      id="toDoForm"
-      on:submit|preventDefault={() => {
-        if (task) {
-          todos.addTodo(task);
-        }
-        task = "";
-      }}
-    >
-      <input
-        bind:value={task}
-        type="text"
-        name="inputText"
-        id="inputText"
-        placeholder="Write a new todo"
-      />
-      <button class="submit" type="submit"><Fa icon={faPlus} /></button>
-    </form>
-    <div class="filters">
-      <button on:click={()=>{$filter='all'}}>All</button>
-      <button on:click={()=>{$filter='completed'}}>Completed</button>
-      <button on:click={()=>{$filter='uncompleted'}}>Uncompleted</button>
-      </div>
-  </main>
-  <div id="todo_container">
-    <ul id="list_container">
-      {#each $displayTodos as task}
-        <div id="list-task">
-          <li
-            contenteditable="true"
-            bind:textContent={task.text}
-            class={task.completed ? "completed" : ""}
-          >
-            {task.text}
-          </li>
-          <div class="btn">
-            <button on:click={() => (task.completed = !task.completed)}
-              ><Fa icon={faCheck} /></button
-            >
-            <button class="deleteBtn" on:click={todos.deleteTodo(task.id)}
-              ><Fa icon={faTrashAlt} /></button
-            >
-          </div>
-        </div>
-      {/each}
-    </ul>
-  </div>
-  
-  <style>
-    .form  {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding-bottom: 30px;
-    }
-    .submit {
-      background-color: #eb06ff;
-      color: white;
-      border: none;
-      border-radius: 50%;
-      margin: 0;
-      padding: 0.5rem 0.75rem;
-    }
-    #toDoForm {
-      margin-bottom: 15px;
-    }
-    #inputText {
-      background-color: #041955;
-      color: white;
-      border: none;
-      padding: 0.5rem;
-      margin: 15px;
-      font-size: 18px;
-    }
-    .filters button {
-      background-color: #041955;
-      color: white;
-      margin: 0;
-      border: none;
-      border-radius: 25px;
-      width: 130px;
-      padding: 0.5rem 0.75rem;
-      box-shadow: 2px 5px 16px 0px #0B325E;
+  // const addTodo = () => {
+  //   // e.preventDefault();
+  //   if (task)
+  //     $todos = [
+  //       ...$todos,
+  //       { text: task, completed: false, id: Math.random() * 100 },
+  //     ];
+  //   task = "";
+  //   LocalStorage.save($todos);
+  // };
+  // const deleteTodo = (id) => {
+  //   $todos = $todos.filter((task) => task.id !== id);
+  //   LocalStorage.save($todos);
+  // };
+
+  $: console.log($search);
+</script>
+
+<main class="form">
+  <form
+    id="toDoForm"
+    on:submit|preventDefault={() => {
+      if (task) {
+        todos.addTodo(task);
       }
-    #todo_container {
-      background-color: #041955;
-      color: #fff;
-      width: 450px;
-      min-height: 250px;
-      padding: 15px;
-      margin: 0 auto;
-      border-radius: 25px;
-      box-shadow: 2px 5px 16px 0px #0B325E;
-    }
-    #list-task{
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      flex-wrap: nowrap;
-      margin: 10px 0;
-    }
-    #list-task li {
-      list-style: none;
-      background-color: white;
-      padding: 10px;
-      color: black;
-      width: 50%;
-      max-width: 250px;
-      overflow: auto;
-    }
-    .deleteBtn {
-      background: white;
-      border: none;
-      color: darkslateblue;
-      margin: 0 15px;
-    }
-    .completed {
-      text-decoration: line-through;
-      opacity: 0.5;
-    }
-  </style>
-  
+      task = "";
+    }}
+  >
+    <input
+      bind:value={task}
+      type="text"
+      name="inputText"
+      id="inputText"
+      placeholder="Write a new todo"
+    />
+    <button class="submit" type="submit"><Fa icon={faPlus} /></button>
+  </form>
+  <div class="filters">
+    <button
+      on:click={() => {
+        $filter = "all";
+      }}>All</button
+    >
+    <button
+      on:click={() => {
+        $filter = "completed";
+      }}>Completed</button
+    >
+    <button
+      on:click={() => {
+        $filter = "uncompleted";
+      }}>Uncompleted</button
+    >
+  </div>
+  <div class="search">
+    <input id="search" type="text" placeholder="search" bind:value={$search} />
+  </div>
+</main>
+<div id="todo_container">
+  <ul id="list_container">
+    {#each $displayTodos as task}
+      <div id="list-task">
+        <li
+          contenteditable="true"
+          bind:textContent={task.text}
+          class={task.completed ? "completed" : ""}
+        >
+          {task.text}
+        </li>
+        <div class="btn">
+          <button on:click={() => (task.completed = !task.completed)}
+            ><Fa icon={faCheck} /></button
+          >
+          <button class="deleteBtn" on:click={todos.deleteTodo(task.id)}
+            ><Fa icon={faTrashAlt} /></button
+          >
+        </div>
+      </div>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 30px;
+  }
+  .submit {
+    background-color: #eb06ff;
+    color: white;
+    border: none;
+    border-radius: 50%;
+    margin: 0;
+    padding: 0.5rem 0.75rem;
+  }
+  #toDoForm {
+    margin-bottom: 15px;
+  }
+  #inputText,
+  #search {
+    background-color: #041955;
+    color: white;
+    border: none;
+    padding: 0.5rem;
+    margin: 15px;
+    font-size: 18px;
+  }
+  #search {
+    width: 400px;
+  }
+  .search {
+    margin-top: 25px;
+  }
+  .filters button {
+    background-color: #041955;
+    color: white;
+    margin: 0;
+    border: none;
+    border-radius: 25px;
+    width: 130px;
+    padding: 0.5rem 0.75rem;
+    box-shadow: 2px 5px 16px 0px #0b325e;
+  }
+  #todo_container {
+    background-color: #041955;
+    color: #fff;
+    width: 450px;
+    min-height: 250px;
+    padding: 15px;
+    margin: 0 auto;
+    border-radius: 25px;
+    box-shadow: 2px 5px 16px 0px #0b325e;
+  }
+  #list-task {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    margin: 10px 0;
+  }
+  #list-task li {
+    list-style: none;
+    background-color: white;
+    padding: 10px;
+    color: black;
+    width: 50%;
+    max-width: 250px;
+    overflow: auto;
+  }
+  .deleteBtn {
+    background: white;
+    border: none;
+    color: darkslateblue;
+    margin: 0 15px;
+  }
+  .completed {
+    text-decoration: line-through;
+    opacity: 0.5;
+  }
+</style>
--- src/stores.js
+++ src/stores.js
@@ -1,34 +1,54 @@
 import { derived, writable } from "svelte/store";
-// import LocalStorage from "./Localstorage.js";
 
 const todoStores = () => {
-    const { subscribe, update, set } = writable([]);
-    return {
-        subscribe,
-        addTodo: (task) => {
-            update((oldTasks) => [...oldTasks,{ text: task, completed: false, id: Math.random() * 100 }]);
-        },
-        deleteTodo: (id) => {
-            update((oldTasks) => oldTasks.filter((task) => task.id !== id));
-        }
-    };
+  const { subscribe, update, set } = writable([]);
+  return {
+    subscribe,
+    addTodo: (task) => {
+      update((oldTasks) => [
+        ...oldTasks,
+        { text: task, completed: false, id: Math.random() * 100 },
+      ]);
+    },
+    deleteTodo: (id) => {
+      update((oldTasks) => oldTasks.filter((task) => task.id !== id));
+    },
+    set
+  };
 };
 
-export const todos = todoStores();
+function localStore(key, store) {
+  const init = JSON.parse(localStorage.getItem(key));
+
+  function save(key, val) {
+    localStorage.setItem(key, JSON.stringify(val));
+  }
+  if (!init) {
+    save(key, []);
+  } else {
+    store.set(init);
+  }
+  store.subscribe((val) => save(key, val));
+    return store;
+}
+
+export const todos = localStore("todos", todoStores());
+
 
 export const filter = writable("all");
+export const search = writable("");
 
-const displayTodos = derived([todos, filter], ([$todos, $filter]) => {
-    return $todos.filter((task) => {
-        switch($filter){
-            case "completed" :
-                return task.completed;
-            case "uncompleted" :
-                return !task.completed;
-            default:
-                return true;
-        }});
+const displayTodos = derived([todos, filter, search], ([$todos, $filter, $search]) => {
+  return $todos.filter((task) => {
+    switch ($filter) {
+      case "completed":
+        return task.completed && task.text.includes($search);
+      case "uncompleted":
+        return !task.completed && task.text.includes($search);
+      default:
+        return task.text.includes($search);
+    }
+  });
 });
 
-export {displayTodos};
-
+export { displayTodos };
